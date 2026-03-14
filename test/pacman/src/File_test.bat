f0c6594cd0a0eb19523943c1c809810b1c91c4dd
DuyLE2408 <ltkduy2432@clc.fitus.edu.vn>
2026-03-14 16:42:25 +0700

    Add files via upload

---
+++ test/pacman/src/File_test.bat
@@ -0,0 +1,9 @@
+@echo off
+echo DANG CHAY TEST 100 TRAN... VUI LONG DOI...
+echo.
+
+:: Thay đổi thông số ở dòng dưới này theo ý bạn
+py -X utf8 batch_test.py --seek example_student --hide example_student --pacman-speed 2 --maps-file map.txt --games 100
+
+echo.
+pause
